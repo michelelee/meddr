a261145ccf56c0888e83729259f6a6188d87e08b
Ariana Promessi <ariana@promessi.com>
2015-07-18 14:39:12 -0700

    adding to donations over time route and chart on org_landing page

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from jinja2 import StrictUndefined
-from flask import Flask, render_template, redirect, request, flash, session
-from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, redirect, request, flash, session, jsonify
+# from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db, db, Donor, Donation, Org, Campaign
 # from text import send_text
+import json
 
 app = Flask(__name__)
 
@@ -31,17 +32,19 @@ def show_donor_landing():
 @app.route('/org_landing', methods=['GET', 'POST'])
 def show_org_land():
     org_name = Org.query.filter_by(org_name='The Org').first()
-    campaign = Campaign.query.filter_by(name='Coding for Kids').first()
-    return render_template('org_landing.html', org_name=org_name, campaign1=campaign1, campaign2=campaign2, campaign3=campaign3)
+    campaign = Campaign.query.filter_by(campaign_name='Coding for Kids').first()
+    return render_template('org_landing.html', org_name=org_name, 
+        # campaign1=campaign1, campaign2=campaign2, campaign3=campaign3
+        )
 
 
 
 @app.route("/donations_over_time", methods = ['GET'])
 def get_chart_data():
 
-    info_needed = (donation.donation_amt,
-                    donation.donated_at,
-                    donation.campaign_id)
+    # info_needed = (donation.donation_amt,
+    #                 donation.donated_at,
+    #                 donation.campaign_id)
 
     donation_objects = Donation.query.all()
 
@@ -60,11 +63,11 @@ if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
     app.debug = True
-    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+    # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
  
     connect_to_db(app)
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    # DebugToolbarExtension(app)
 
     app.run()
