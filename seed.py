99b3bfaa1912ac98886295b24afc0f8bc92cfd81
Ariana Promessi <ariana@promessi.com>
2015-07-18 12:55:44 -0700

    completed seed file and seeded DB with random data

--- seed.py
+++ seed.py
@@ -15,16 +15,73 @@ def create_donors():
 					political_aff = random.choice(political_parties))
 
 	db.session.add(a_donor)
-	db.session.commit()
 
 def create_donations():
-	a_donation = (donated_at
+	donated_at = forgery_py.forgery.date.date(past=True, min_delta=0, max_delta=600)
+	donation_amt = random.randint(5, 2000)
+	donor_id = random.randrange(1, 100)
+	org_id = random.randrange(1, 5)
+	campaign_id = random.randrange(1, 3)
+
+	a_donation = Donation(donated_at = donated_at,
+							donation_amt = donation_amt,
+							donor_id = donor_id,
+							org_id = org_id,
+							campaign_id = campaign_id)
+
+	db.session.add(a_donation)
 
 def create_orgs():
-	pass
+	
+	org_name = "Coding for Kids"
+	password = forgery_py.forgery.basic.text(length=None,
+			at_least=3, at_most=15, lowercase=True, uppercase=True, 
+			digits=False, spaces=False, punctuation=False)
+	org_url = forgery_py.forgery.internet.top_level_domain()
+
+	an_org = Org(org_name = org_name,
+				password = password,
+				org_url = org_url)
+
+	db.session.add(an_org)
+
+	def random_orgs():
+		org_name = forgery_py.forgery.basic.text(length=None,
+			at_least=3, at_most=15, lowercase=True, uppercase=True, 
+			digits=False, spaces=True, punctuation=False)
+		password = forgery_py.forgery.basic.text(length=None,
+			at_least=3, at_most=15, lowercase=True, uppercase=True, 
+			digits=False, spaces=False, punctuation=False)
+		org_url = forgery_py.forgery.internet.top_level_domain()
+
+		an_org = Org(org_name = org_name,
+					password = password,
+					org_url = org_url)
+
+		db.session.add(an_org)
+
+		for i in range(4):
+			random_orgs()
+
 
 def create_campaigns():
-	pass
+	stem = Campaign(campaign_name = "STEM Program for Girls",
+		target_amt = 50000,
+		description = forgery_py.forgery.lorem_ipsum.paragraph(separator='\n\n', wrap_start='', wrap_end='', html=False, sentences_quantity=3),
+		issue_area = "Education")
+	hour_of_code = Campaign(campaign_name = "Hour of Code",
+		target_amt = 10000,
+		description = forgery_py.forgery.lorem_ipsum.paragraph(separator='\n\n', wrap_start='', wrap_end='', html=False, sentences_quantity=3),
+		issue_area = "Education")
+	higher_ed = Campaign(campaign_name = "Road to Higher Education",
+		target_amt = 20000,
+		description = forgery_py.forgery.lorem_ipsum.paragraph(separator='\n\n', wrap_start='', wrap_end='', html=False, sentences_quantity=3),
+		issue_area = "Education")
+
+	db.session.add(stem)
+	db.session.add(hour_of_code)
+	db.session.add(higher_ed)
+
 
 
 if __name__ == "__main__":
@@ -32,4 +89,12 @@ if __name__ == "__main__":
 
 	for i in range(100):
 		create_donors()
+
+	for i in range(150):
+		create_donations()
+
+	create_orgs()
+	create_campaigns()
+
+	db.session.commit()
 
