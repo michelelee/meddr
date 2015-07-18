772ef514fa717ee21745e016785baa11c6d5b391
Ariana Promessi <ariana@promessi.com>
2015-07-17 23:01:57 -0700

    built out model

--- model.py
+++ model.py
@@ -7,59 +7,61 @@ db = SQLAlchemy()
 
 
 class Donor(db.Model):
-    """Drug info from openFDA"""
+    """Individual donors that donate to campaigns and 501(c)s"""
 
-    __tablename__="drugs"
+    __tablename__="donors"
 
     donor_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String(99999), nullable=False)
-    password = db.Column(db.String(64), nullable=True)
+    # password = db.Column(db.String(64), nullable=True)
     age = db.Column(db.Integer, nullable=True)
     phone = db.Column(db.String(10), nullable=False)
     zipcode = db.Column(db.String(15), nullable=True)
-    jobtitle = db.Column(db.String(99999), nullable=False)
+    jobtitle = db.Column(db.String(500), nullable=False)
+    political_aff = db.Column(db.String(500), nullable=False)
 
     def __repr__(self):
-        """Provide helpful representation when printed."""
-        return "<spl_set_id=%s generic_name=%s brand_name=%s>" % (self.spl_set_id, self.generic_name, self.brand_name)
+        return "<Donor: name=%s>" % (self.name)
 
 class Org(db.Model):
-    """User of memed website."""
+    """501(c) organizations - recipients of donations"""
 
-    __tablename__ = "users"
+    __tablename__ = "orgs"
 
     org_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     org_name = db.Column(db.String(64), nullable=True)
     password = db.Column(db.String(64), nullable=True)
     org_url = db.Column(db.String(64), nullable=True)
-  
-
-    @staticmethod
-    def get_user(org_id):
-        user = User.query.filter_by(org_id=org_id).first()
-        print  user
-        print "in get_user static method"
-        return user
-
 
     def __repr__(self):
-        """Provide helpful representation when printed."""
-
-        return "<User user_id=%s email=%s>" % (self.user_id, self.meddr_username)
+        return "<Org: org_name=%s>" % (self.org_name)
 
 
-class Rating(db.Model):
-    """Rating of a drug by a user."""
+class Donation(db.Model):
+    """Donation objects - links together donors, 501(c)s, and campaigns"""
 
-    __tablename__ = "ratings"
+    __tablename__ = "donations"
+
+    donation_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    donated_at = db.Column(db.DateTime(64), nullable=True)
+    donor_id = db.Column(db.Integer, db.ForeignKey("donors.donor_id"))
+    org_id = db.Column(db.Integer, db.ForeignKey("orgs.org_id"))
+    campaign_id = db.Column(db.Integer, db.ForeignKey("campaigns.campaign_id"))
+
+    def __repr__(self):
+        return "<Donation: donation_id=%s, donated_at=%s>" % (self.donation_id, self.donated_at)
+
+
+class Campaign(db.Model):
+    """Campaigns that organizations can support"""
+
+    __tablename__ = "campaigns"
+
+    campaign_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    target_amt = db.Column(db.Float, nullable = False)
+    description = db.Column(db.String(99999))
+    issue_area = db.Column(db.String(100))
 
 
 
 
-    __table_args__ = (CheckConstraint(score.in_(range(1, 11))), )
-
-    user = db.relationship("User",
-                           backref=db.backref("ratings", order_by=rating_id))
-
-    drug = db.relationship("Drug",
-                            backref=db.backref("ratings", order_by=rating_id))
